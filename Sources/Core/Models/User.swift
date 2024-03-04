0c460db80317d1f1f2b273c2f5edb178de10e427
Uwe Tilemann <dehale@me.com>
2024-03-04 13:26:14 +0100

    add consent to user

--- Sources/Core/Models/User.swift
+++ Sources/Core/Models/User.swift
@@ -12,12 +12,14 @@ public struct User: Decodable, Identifiable {
     public let phoneNumber: String?
     public let details: Details?
     public let fields: [Field]?
+    public let consent: Consent?
     
     enum CodingKeys: String, CodingKey {
         case id
         case phoneNumber
         case details
         case fields = "detailFields"
+        case consent = "currentConsent"
     }
     
     public init(from decoder: Decoder) throws {
@@ -26,8 +28,9 @@ public struct User: Decodable, Identifiable {
         self.phoneNumber = try container.decodeIfPresent(String.self, forKey: .phoneNumber)
         self.details = try container.decodeIfPresent(Details.self, forKey: .details)
         self.fields = try container.decodeIfPresent([Field].self, forKey: .fields)
+        self.consent = try container.decodeIfPresent(Consent.self, forKey: .consent)
     }
-
+    
     public struct Details: Codable, Equatable {
         public let firstName: String?
         public let lastName: String?
@@ -47,6 +50,18 @@ public struct User: Decodable, Identifiable {
         enum CodingKeys: String, CodingKey {
             case id
             case isRequired = "required"
+        }
+    }
+    
+    public struct Consent: Decodable, Equatable {
+        public let major: Int
+        public let minor: Int
+        
+        public static func < (lhs: Self, rhs: Self) -> Bool {
+            lhs.major < rhs.major
+        }
+        public static func == (lhs: Self, rhs: Self) -> Bool {
+            lhs.major == rhs.major && lhs.minor == rhs.minor
         }
     }
 }
