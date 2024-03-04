0e5257b9543faae15b0dd770961ba7ac0b307a88
Uwe Tilemann <dehale@me.com>
2024-03-04 13:18:43 +0100

    Revert "cdd consent to user"
    This reverts commit 4d9d3293d6cd289dec2982d3fe461f392eb97b55.

--- Sources/Core/Models/User.swift
+++ Sources/Core/Models/User.swift
@@ -12,23 +12,20 @@ public struct User: Decodable, Identifiable {
     public let phoneNumber: String?
     public let details: Details?
     public let fields: [Field]?
-    public let consent: Consent?
-
+    
     enum CodingKeys: String, CodingKey {
         case id
         case phoneNumber
         case details
         case fields = "detailFields"
-        case consent = "currentConsent"
     }
-
+    
     public init(from decoder: Decoder) throws {
         let container: KeyedDecodingContainer<CodingKeys> = try decoder.container(keyedBy: CodingKeys.self)
         self.id = try container.decode(String.self, forKey: .id)
         self.phoneNumber = try container.decodeIfPresent(String.self, forKey: .phoneNumber)
         self.details = try container.decodeIfPresent(Details.self, forKey: .details)
         self.fields = try container.decodeIfPresent([Field].self, forKey: .fields)
-        self.consent = try container.decodeIfPresent(Consent.self, forKey: .consent)
     }
 
     public struct Details: Codable, Equatable {
@@ -42,7 +39,7 @@ public struct User: Decodable, Identifiable {
             self.email = email
         }
     }
-
+    
     public struct Field: Decodable, Identifiable, Equatable {
         public let id: String
         public let isRequired: Bool
@@ -50,18 +47,6 @@ public struct User: Decodable, Identifiable {
         enum CodingKeys: String, CodingKey {
             case id
             case isRequired = "required"
-        }
-    }
-
-    public struct Consent: Decodable, Equatable {
-        public let major: Int
-        public let minor: Int
-        
-        public static func < (lhs: Self, rhs: Self) -> Bool {
-            lhs.major < rhs.major
-        }
-        public static func == (lhs: Self, rhs: Self) -> Bool {
-            lhs.major == rhs.major && lhs.minor == rhs.minor
         }
     }
 }
